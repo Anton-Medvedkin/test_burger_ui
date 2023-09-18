d42df6eb3e8c02787b245de1a1b7751b30313622
37533_xdzegeo <medvedkin.antonQA@yandex.by>
2023-09-18 16:34:24 +0300

    Adding time in click_button_sauces

--- test/BurgerConstructor_test.py
+++ test/BurgerConstructor_test.py
@@ -5,12 +5,15 @@ import allure
 def test_navigating_through_sections(browser):
     main_constructor_page = ConstructorPage(browser)
     main_constructor_page.go_to_constructor_page()
-    main_constructor_page.click_button_sauces()
-    assert main_constructor_page.find_element(ConstructorLocators.LOCATOR_CONSTRUCTOR_PAGE_CAPTION_SAUCES)
+    main_constructor_page.click_button_toppings()
+    assert main_constructor_page.find_element(ConstructorLocators.LOCATOR_CONSTRUCTOR_PAGE_CAPTION_TOPPINGS)
     main_constructor_page.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     main_constructor_page.click_button_buns()
     assert main_constructor_page.find_element(ConstructorLocators.LOCATOR_CONSTRUCTOR_PAGE_CAPTION_BUNS)
-    main_constructor_page.click_button_toppings()
-    assert main_constructor_page.find_element(ConstructorLocators.LOCATOR_CONSTRUCTOR_PAGE_CAPTION_TOPPINGS)
+    main_constructor_page.click_button_sauces()
+    assert main_constructor_page.find_element(ConstructorLocators.LOCATOR_CONSTRUCTOR_PAGE_CAPTION_SAUCES)
+    main_constructor_page.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+
 
 
