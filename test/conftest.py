a1a205794f217c293017f1a8b82edd7bdc72b2fe
37533_xdzegeo <medvedkin.antonQA@yandex.by>
2023-09-18 12:04:53 +0300

    Updating conftest

--- test/conftest.py
+++ test/conftest.py
@@ -12,12 +12,14 @@ def browser(request):
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--headless")
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     elif request.param == "yandex":
         options = webdriver.ChromeOptions()
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--headless")
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     else:
         raise ValueError("Неподдерживаемый браузер")
